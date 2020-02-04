2dd968a65aff9293d4d53a98d08e54a24d628024
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-02-03 19:23:14 -0700

    deleted extra src folders and merged into one structured folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import './App.css';
-import RequestsCard from './components/RequestsCard';
-import Requests from './components/Requests';
+import React from "react";
+import "./App.css";
+import RequestsCard from "./components/RequestsCard";
+import Requests from "./components/Requests";
 
 function App() {
-  return (
-    <div className="App">
-      <RequestsCard />
-      
-    </div>
-  );
+ return (
+  <main>
+   <nav>
+    <p className="future-image">The "logo" will be here</p>
+    <h1>Replate</h1>
+    <h2>Logins</h2>
+   </nav>
+   <LoginForms />
+  </main>
+ );
 }
 
 export default App;
